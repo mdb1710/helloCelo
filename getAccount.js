4aaf6b6daa35ec9e75031d0f571f2c713d8f521f
mdb1710 <mdb1710@gmail.com>
2020-03-03 08:13:23 -0500

    test

--- getAccount.js
+++ getAccount.js
@@ -1,27 +1,29 @@
-const Web3 = require('web3')
-const fs = require('fs');
+"use strict";
+
+const Web3 = require("web3");
+const fs = require("fs");
 var web3 = new Web3();
 
 function getAccount() {
-    return new Promise(resolve => {
-        fs.readFile('./.secret', {encoding: 'utf-8'}, (err, data) => {
-            if(data.length == 0){
-                let randomAccount = web3.eth.accounts.create()
-        
-                fs.writeFile("./.secret", randomAccount.privateKey, (err) => {
-                    if(err) {
-                        return console.log(err);
-                    }
-                })
+  return new Promise(resolve => {
+    fs.readFile("./.secret", { encoding: "utf-8" }, (err, data) => {
+      if (data.length === 0) {
+        let randomAccount = web3.eth.accounts.create();
 
-                resolve(randomAccount)
-            } else {
-                resolve(web3.eth.accounts.privateKeyToAccount(data))
-            }
-        })
-    })
+        fs.writeFile("./.secret", randomAccount.privateKey, err => {
+          if (err) {
+            return console.log(err);
+          }
+        });
+
+        resolve(randomAccount);
+      } else {
+        resolve(web3.eth.accounts.privateKeyToAccount(data));
+      }
+    });
+  });
 }
 
 module.exports = {
-    getAccount
-}
+  getAccount
+};
--- helloCello.js
+++ helloCello.js
@@ -1,8 +1,21 @@
 // add package imports and setup up here
 
-async function awaitWrapper(){
-    // add async/await code in this function
+const Kit = require("@celo/contractkit");
 
+const kit = Kit.newKit("https://alfajores-forno.celo-testnet.org");
+
+const getAccount = require("./getAccount").getAccount;
+
+let anAddress = "0xD86518b29BB52a5DAC5991eACf09481CE4B0710d";
+
+async function awaitWrapper() {
+  // add async/await code in this function
+  let account = await getAccount();
+  let goldtoken = await kit.contracts.getGoldToken();
+  let balance = await goldtoken.balanceOf(anAddress);
+
+  console.log(account.address);
+  console.log(balance.toString());
 }
 
-awaitWrapper()
+awaitWrapper();
